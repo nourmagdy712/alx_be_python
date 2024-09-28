d1566b4e8b197ffa7e081b9240f737730dda25bc
Nour Magdy <nourmagdy574@gmail.com>
2024-09-28 17:54:50 +0300

    adding temp_conversion_tool

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,3 +1,17 @@
+"""Part 1: Display the Current Date and Time
+
+Research how to use the datetime module to obtain the current date and time.
+Create a function with a name display_current_datetime and
+save the current date inside a current_date variable
+Format and print the current date and time in a readable format, such as “YYYY-MM-DD HH:MM:SS”.
+Part 2: Calculate a Future Date
+
+Prompt the user to enter a number of days (as an integer).
+Create a function with a name calculate_future_date and
+saves the future date inside a future_date variable
+Calculate what the date will be after adding the specified number of days to the current date.
+Print the future date in a format like “YYYY-MM-DD”."""
+
 from datetime import datetime, timedelta
 
 def display_current_datetime():
