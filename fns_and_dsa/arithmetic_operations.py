d3dda4208e12a6e8d2efa6e9cd4d782a0803ebfb
Nour Magdy <nourmagdy574@gmail.com>
2024-09-28 16:52:12 +0300

    updates

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,7 +6,7 @@ Return the result of the arithmetic operation."""
 
 
 
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
